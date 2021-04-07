241d8f727d3f2ec8e00e60d78262a15c91acb267
megan cho <meganlisacho@gmail.com>
2021-04-07 15:53:29 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  let result
+  if (someNumber <= 400) {
+    result = 'This one is on me!';
+  }
+  if (someNumber >= 2000) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  if (someNumber >= 2500) {
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  return (someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(someValue){
+  let result
+  switch (someValue) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    case 'thanks for everything':
+      result = 'Bye.'
+      break;
+  }
+  return result;
 }
